6795aa6bc98f5a3256805fbbbd4ef9dab210b235
016172 <seolmi@hotmail.se>
2020-06-08 16:15:35 +0200

    solved

--- problems/polygonarea.py
+++ problems/polygonarea.py
@@ -9,6 +9,5 @@ while True:
             sum += int(points[y][0]) * int(points[y+1][1]) - int(points[y][1]) * int(points[y+1][0])
         sum += int(points[len(points)-1][0]) * int(points[0][1]) - int(points[len(points)-1][1]) * int(points[0][0])
         print("CW" + " " + str(abs(sum/2))) if sum < 0 else print("CCW" + " " + str(abs(sum/2)))
-
     else:
         break
