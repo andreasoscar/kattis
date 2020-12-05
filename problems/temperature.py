cf499db7371c653d9e6eef473c7848c990c84e8f
016172 <seolmi@hotmail.se>
2020-12-05 01:02:27 +0100

    solution

--- problems/temperature.py
+++ problems/temperature.py
@@ -1,18 +1,14 @@
 x,y = [int(t) for t in input().split(" ")]
 ta = 0
 tb = x
-x1 = 0
 if x == 0 and y == 1:
     print("ALL GOOD")
-elif y > 1:
-    if abs(x) == abs(y):
+elif y == 1 and x != 0:
+    print("Impossible")
+else:
+    if x == y:
         print("Impossible")
     else:
-        x1 = x/(y-1)
-        print(x1,y*x1)
-        tb = tb - y*x1
-        print(float(tb))
-else:
-    print("här")
-    if x != 0:
-        print("Impossible")
+        x1 = (ta-tb)/(1-y)
+        ta = ta - x1
+    print(ta)
