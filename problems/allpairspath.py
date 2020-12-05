9677411b01f30c18c36c93ff2d3fea38007da1b8
016172 <seolmi@hotmail.se>
2020-12-05 12:01:12 +0100

    wrong answer

--- problems/allpairspath.py
+++ problems/allpairspath.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sys
 def dijkstra(graph,source,neighbors):
     Q = set()
@@ -11,49 +12,44 @@ def dijkstra(graph,source,neighbors):
         Q.add(v)
     dist[source] = 0
     while len(Q) > 0:
-        newDist = []
+        newDist = dict()
         for i in dist.items():
             if i[0] in Q:
-                newDist.append(i)
-        min = float('inf')
-        u = -1
-        for i in newDist:
-            if min > i[1] and i[0] in neighbors.keys():
-                min = i[1]
-                u = i[0]
-        if u == -1:
+                newDist[i[0]] = i[1]
+        if len(newDist) == 0:
             break
-        elif u >= 0:
-            Q.remove(u)
-            for i in neighbors[u]:
-                if i in Q:
-                    dis = dist[u] + graph[(u,i)]
-                    if dis < dist[i]:
-                        dist[i] = dis
-                        prev[i] = u
+        u = min(newDist,key=newDist.get)
+        Q.remove(u)
+        for i in neighbors[u]:
+            dis = dist[u] + graph[(u,i)]
+            if dis < dist[i]:
+                dist[i] = dis
+                prev[i] = u
     return dist
 while True:
     n,m,q = [int(i) for i in input().split(" ")]
     if n == 0 and m == 0 and q == 0:
         break
     else:
-        neighbors = dict()
+        neighbors = [[]]*n
         paths = dict()
         queries = []
         for i in range(m):
             s,t,c = [int(i) for i in input().split(" ")]
             paths[(s,t)] = c
-            if s in neighbors.keys():
-                neighbors[s] = neighbors[s] + [t]
-            else:
-                neighbors[s] = [t]
+            neighbors[s] = neighbors[s] + [t]
         for j in range(q):
             s,t = [int(i) for i in input().split(" ")]
             queries.append([s,t])
+        ways = dijkstra(paths,0,neighbors)
         for i in queries:
-            ways = dijkstra(paths,i[0],neighbors)
-            if i[1] not in ways.keys() or ways[i[1]] == 'inf':
-                print("Impossible")
-            else:
+            if i[0] == i[1]:
+                print("0")
+            elif i[0] == 0 and i[1] in ways:
                 print(ways[i[1]])
+            elif i[0] != 0:
+                if (i[0],i[1]) in paths and i[1] in ways:
+                    print(abs((ways[i[1]] - paths[(i[0],i[1])])))
+                else:
+                    print("Impossible")
         print("\n")
