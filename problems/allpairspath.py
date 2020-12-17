bfb4b4040d620b03a88140977c671d76993daec6
016172 <seolmi@hotmail.se>
2020-12-18 00:32:02 +0100

    still working on it

--- problems/allpairspath.py
+++ problems/allpairspath.py
@@ -1,32 +1,45 @@
 inf = float('inf')
 import copy
-def warshall(G,k):
-    print(G)
-    distance = copy.copy(G)
-    for i in range(k):
-        for j in range(k):
-            for v in range(k):
-                distance[j][v] = min(distance[j][v],distance[j][i] + distance[i][v])
-    return distance
+def warshall(G,t):
+    d = copy.deepcopy(G)
+    print(d.keys())
+    for c in G:
+        for j in d.keys():
+            if not j in d[c].keys():
+                if c == j:
+                    d[c].update({c:0})
+                else:
+                    d[c].update({j:inf})
+    print(d)
+    for i in range(t):
+        for j in range(t):
+            for k in range(t):
+                print(d)
+                print(i,j,k)
+                d[i][j] = min(d[i][j],d[i][k] + d[k][j])
+    # for i in range(t):
+    #     for j in range(t):
+    #         for k in range(t):
+    #             if d[k][k] < 0 and d[i][k] != inf and d[k][j] != inf:
+    #                 d[i][j] = -inf
+#    return d
 
 while True:
     n,m,q = [int(i) for i in input().split(" ")]
     if n == 0 and m == 0 and q == 0:
         break
     else:
-        sy = set()
-        graph,queries = [[inf,inf,inf,inf]]*n,[]
+        pairs,dir = dict(),[]
         for i in range(m):
             s,t,c = [int(i) for i in input().split()]
-            graph[s].insert(t,c)
-        paths = (warshall(graph,len(sy)))
-        for i in range(q):
-            s,t = [int(_) for _ in input().split()]
-            if paths[s][t] == inf:
-                print("Impossible")
+            tmp = dict()
+            if s in pairs.keys():
+                tmp[t] = c
+                pairs[s] = pairs[s] + tmp
             else:
-                if s == t:
-                    print(0)
-                else:
-                    print(paths[s][t])
-        print("\n")
+                tmp[t] = c
+                pairs[s] = tmp
+        for i in range(q):
+            s,t = [int(i) for i in input().split()]
+            dir.append([s,t])
+        print(warshall(pairs,len(pairs)))
