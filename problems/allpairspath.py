d97ecc0c8a0979ae4486527e57ef06a2b9f4dae8
016172 <seolmi@hotmail.se>
2020-12-19 00:19:25 +0100

    runtime error on second case, cant figure out why

--- problems/allpairspath.py
+++ problems/allpairspath.py
@@ -1,45 +1,47 @@
 inf = float('inf')
-import copy
-def warshall(G,t):
-    d = copy.deepcopy(G)
-    print(d.keys())
-    for c in G:
-        for j in d.keys():
-            if not j in d[c].keys():
-                if c == j:
-                    d[c].update({c:0})
-                else:
-                    d[c].update({j:inf})
-    print(d)
+inf1 = -inf
+def warshall(G):
+    d,t = G,len(G)
+
+    #print(d,t)
     for i in range(t):
         for j in range(t):
             for k in range(t):
-                print(d)
-                print(i,j,k)
                 d[i][j] = min(d[i][j],d[i][k] + d[k][j])
     # for i in range(t):
     #     for j in range(t):
     #         for k in range(t):
     #             if d[k][k] < 0 and d[i][k] != inf and d[k][j] != inf:
-    #                 d[i][j] = -inf
-#    return d
+    #                 d[i][j] = inf1
+    return d
 
 while True:
     n,m,q = [int(i) for i in input().split(" ")]
     if n == 0 and m == 0 and q == 0:
         break
     else:
-        pairs,dir = dict(),[]
+        edges,dir,g = [],[],set()
         for i in range(m):
             s,t,c = [int(i) for i in input().split()]
-            tmp = dict()
-            if s in pairs.keys():
-                tmp[t] = c
-                pairs[s] = pairs[s] + tmp
-            else:
-                tmp[t] = c
-                pairs[s] = tmp
-        for i in range(q):
+            edge = [inf]*(n+1)
+            edge[t] = c
+            edges.insert(i,edge)
+            g.add(s)
+        for v in set(range(0,n)).difference(g):
+            edges.insert(v,[inf]*(n+1))
+        for j in range(q):
             s,t = [int(i) for i in input().split()]
             dir.append([s,t])
-        print(warshall(pairs,len(pairs)))
+        sp = warshall(edges)
+        for v in dir:
+            s,t = v[0],v[1]
+            if s < len(sp) and t < len(sp):
+                if s == t:
+                    print(0)
+                elif sp[s][t] == inf:
+                    print("Impossible")
+                elif sp[s][t] == inf1:
+                    print("-Infinity")
+                else:
+                    print(sp[s][t])
+        print("\n")
