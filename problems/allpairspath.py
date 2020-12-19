7bfa4cfe744932e9079e4f8056026f32c6a8b5c6
Andreas Olsson <andreasolsson@MacBook-Pro-som-tillhor-Andreas.local>
2020-12-20 00:36:31 +0100

    input parsing was the problem, solved

--- problems/allpairspath.py
+++ problems/allpairspath.py
@@ -1,47 +1,38 @@
-inf = float('inf')
-inf1 = -inf
-def warshall(G):
-    d,t = G,len(G)
-
-    #print(d,t)
+INF = float('inf')
+def warshall(G,t):
     for i in range(t):
         for j in range(t):
             for k in range(t):
-                d[i][j] = min(d[i][j],d[i][k] + d[k][j])
-    # for i in range(t):
-    #     for j in range(t):
-    #         for k in range(t):
-    #             if d[k][k] < 0 and d[i][k] != inf and d[k][j] != inf:
-    #                 d[i][j] = inf1
-    return d
+                if G[j][i] < INF and G[i][k] < INF:
+                    G[j][k] = min(G[j][k],G[j][i] + G[i][k])
+    for i in range(t):
+        for j in range(t):
+            for k in range(t):
+                if G[i][j] != -INF:
+                    if G[k][k] < 0 and G[i][k] != INF and G[k][j] != INF:
+                        G[i][j] = -INF
+
 
 while True:
     n,m,q = [int(i) for i in input().split(" ")]
     if n == 0 and m == 0 and q == 0:
         break
     else:
-        edges,dir,g = [],[],set()
+        matrix = [[0 if i == j else INF for i in range(n)] for j in range(n)]
         for i in range(m):
-            s,t,c = [int(i) for i in input().split()]
-            edge = [inf]*(n+1)
-            edge[t] = c
-            edges.insert(i,edge)
-            g.add(s)
-        for v in set(range(0,n)).difference(g):
-            edges.insert(v,[inf]*(n+1))
+            s,t,c = [int(_) for _ in input().split()]
+            if s == t:
+                matrix[s][t] = 0
+            elif matrix[s][t] > c:
+                matrix[s][t] = c
+        warshall(matrix,n)
         for j in range(q):
-            s,t = [int(i) for i in input().split()]
-            dir.append([s,t])
-        sp = warshall(edges)
-        for v in dir:
-            s,t = v[0],v[1]
-            if s < len(sp) and t < len(sp):
-                if s == t:
-                    print(0)
-                elif sp[s][t] == inf:
-                    print("Impossible")
-                elif sp[s][t] == inf1:
-                    print("-Infinity")
-                else:
-                    print(sp[s][t])
-        print("\n")
+            s,t = [int(_) for _ in input().split()]
+            c = matrix[s][t]
+            if c == -INF:
+                print("-Infinity")
+            elif c == INF:
+                print("Impossible")
+            else:
+                print(c)
+        print()
