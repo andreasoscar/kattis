309af9361b0890ff89abff0b8b5d2d6b3b9923d7
016172 <seolmi@hotmail.se>
2021-01-01 22:40:42 +0100

    working on beautifulprimes.py

--- problems/beautifulprimes.py
+++ problems/beautifulprimes.py
@@ -1,27 +1,29 @@
 import math
 n = int(input())
+v = 78498
 data = []
 for i in range(n):
     data.append(int(input()))
-A = [True]*(78498)
+A = [True]*(v+1)
 A[0] = A[1] = False
-for i in range(2,int(math.sqrt(78498))+1):
+for i in range(2,int(math.sqrt(v+1))+1):
     if A[i]:
-        for j in range(i**i,78498,i):
+        for j in range(i*2,v+1,i):
             A[j] = False
 for j in data:
     prod = 1
     primes = []
     if j > 1:
-        for i in range(len(A)):
-            if A[i]:
-                if len(str(prod)) < j and len(primes) >= j:
-                    del primes[0]
-                elif len(str(prod*i)) <= j:
-                    primes.append(i)
-                    prod = prod * i
-                elif len(str(prod)) == j:
-                    break
+        i = 0
+        while len(str(prod)) < j:
+            if len(primes) == j-1 and len(str(prod*i)) < j:
+                del primes[0]
+            elif A[i]:
+                prod = prod * i
+                primes.append(i)
+                i += 1
+            else:
+                i += 1
         print(*primes)
-    else:
-        print("5")
+    elif j == 1:
+        print("3")
