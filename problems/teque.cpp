e0aee8dbe9cfad13d9b95193d590fc65848c0f18
andreasoscar <seolmi@hotmail.se>
2021-01-12 00:12:59 +0100

    teque solved in c++

--- problems/teque.cpp
+++ problems/teque.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+#include <deque>
 #include <iostream>
 #include <list>
 #include <string>
@@ -7,8 +7,8 @@ using namespace std;
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  std::vector<int> first;
-  std::vector<int> last;
+  std::deque<int> first;
+  std::deque<int> last;
   int n;
   string s;
   int n1;
@@ -17,19 +17,27 @@ int main(){
     cin >> s >> n1;
     if(s == "push_back"){
       last.push_back(n1);
+      if(last.size() > first.size()){
+        first.push_back(last.front());
+        last.pop_front();
+      }
+    
     } else if(s == "push_front"){
-        first.insert(first.begin(),n1);
+      first.push_front(n1);
+      if(first.size() > (last.size() + 1)){
+        last.push_front(first.back());
+        first.pop_back();
+      }
     } else if(s == "push_middle"){
-        if(first.size() == last.size()){
-            first.insert(first.end(),n1);
-        } else if(first.size() >= last.size()){
-            last.insert(last.begin(),n1);
-            //3,5,9 -> 3,5,1,9 -> 3,5,2500,1,9 -> 3,5,2500,1,9,9999 -> 3,5,2500,1,9,9999,250 -> -1,3,5,2500,25000,1,9,9999,250 ->  
-        }
+      if(first.size() > last.size()){
+        last.push_front(n1);
+      } else {
+        first.push_back(n1);
+      }
     } else if(s=="get"){
       if(n1 < first.size()){
         cout << first[n1] << endl;
-      } else if(n1 >= first.size() and n1 <(first.size() + last.size())){
+      } else if(n1 >= first.size() && n1 <= (first.size()+last.size())){
         cout << last[n1-first.size()] << endl;
       }
     }
