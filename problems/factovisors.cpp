00bb96df53d0644b1bb137b65872d1b294f525d0
andreasoscar <seolmi@hotmail.se>
2021-01-12 00:47:41 +0100

    factovisors

--- problems/factovisors.cpp
+++ problems/factovisors.cpp
@@ -10,18 +10,7 @@ int main(){
   std::vector<int> pf;
   int n,m;
   cin >> n >> m;
-  while(n%2 == 0){
-      pf.push_back(2)
-      n = n/2;
-  }
-  for(int k=3; k <= sqrt(n);k = k + 2){
-      while(n%k == 0){
-          pf.push_back(k);
-      }
-  }
-  if(n>2){
-      pf.push_back(n);
-  }
+  
   
 
 }
