4e327d109f21bd5c0f21b4ccb6691895c8c89fb6
Andreas Olsson <andreasolsson@MacBook-Pro-som-tillhor-Andreas.local>
2021-01-15 14:48:38 +0100

    solved

---
+++ problems/armystrengthhard/armystrengthhard.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# problem: armystrengthhard, rating: 2.2
+from collections import deque
+n = int(input())
+a1,a2 = deque(),deque()
+for i in range(n):
+    t,v = input(),input()
+    a1 = deque(sorted([int(_) for _ in input().split()]))
+    a2 = deque(sorted([int(_) for _ in input().split()]))
+    while len(a1) and len(a2) > 0:
+        if a1[0] > a2[0]:
+            a2.popleft()
+        elif a1[0] < a2[0]:
+            a1.popleft()
+        elif a1[0] == a2[0]:
+            a2.popleft()
+    if len(a1) > len(a2):
+        print("Godzilla")
+    else:
+        print("MechaGodzilla")
